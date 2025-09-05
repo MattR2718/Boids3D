5c4baaac56932cf4e6d5104d72e05d95506ee122
MattR2718 <matthew@ranson2718.co.uk>
2025-09-05 15:51:14 +0100

    create pipeline class

---
+++ Boids3D/Pipeline.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/*
+
+	Responsibilities: Encapsulates the Vulkan graphics pipeline state.
+
+	Owns: vk::raii::Pipeline, vk::raii::PipelineLayout, vk::raii::DescriptorSetLayout.
+
+	Manages: Loading shaders, defining vertex input, and configuring all pipeline stages (rasterization, color blending, depth testing, etc.). 
+			A Pipeline would be configured for a specific type of material or rendering effect.
+
+*/
+
+class Pipeline {
+public:
+
+
+private:
+
+
+
+};
