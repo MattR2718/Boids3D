ee6911c703747a8e866af44e8fc66252af36e2b5
MattR2718 <matthew@ranson2718.co.uk>
2025-09-08 15:27:26 +0100

    add shader compilation build step

---
+++ Boids3D/compile_shaders.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Args:
+:: %1 = Shader source directory
+:: %2 = Build shaders directory
+
+set SHADER_DIR=%~1
+set BUILD_DIR=%~2
+
+:: Make sure the output directory exists
+if not exist "%BUILD_DIR%" (
+    mkdir "%BUILD_DIR%"
+)
+
+:: Loop over all .slang files in SHADER_DIR
+for %%f in ("%SHADER_DIR%\*.slang") do (
+    echo Compiling %%~nxf...
+    slangc "%%f" -o "%BUILD_DIR%\%%~nf.spv"
+)
+
+echo.
+echo All shaders compiled to: %BUILD_DIR%
+endlocal
